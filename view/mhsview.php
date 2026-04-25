9c145307c81a11ad038a7376ed8022ae4376c45c
Aurella Meidista Septiasa <aurellinda703@gmail.com>
2026-04-25 21:41:35 +0700

    buat mhs view

---
+++ view/mhsview.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+
+<h2>Mahasiwa</h2>
+
+<ul>
+<?php foreach($data as $mhs): ?>
+    <li><?= $mhs; ?></li>
+<?php endforeach; ?>
+</ul>
+
+</body>
+</html>
